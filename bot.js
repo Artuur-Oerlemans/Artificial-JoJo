f101e84e2bc383aad6c7d874e6ccf16bd8fd852f
artuur <artuur.oerlemans@gmail.com>
2018-12-19 10:06:01 +0100

    Gave shoutOra it's own file

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const token = "NTE4MzQ0Mjg3NTU0MTA5NDUw.DuQYpw.Ibtms2TwuW1agDtb2-tQ-HL9Jl4";
 var client = new discord.Client();
 client.memory = require("./memory.json");
 const Currency = require("./currency.js");
+const shoutOra = require("./shoutOra.js");
 
 
 client.login(token);
@@ -250,35 +251,6 @@ function updateInventory(contributor, goods, change) {
         if (err) throw err;
     });
     return afterTransfer;
-}
-
-// shout a battle cry a number of times
-function shoutOra(channel, timesNotParsed) {
-    let times = parseInt(timesNotParsed);
-    if (timesNotParsed == "e" || timesNotParsed == "pi") {
-        channel.send("なにィ！！");
-        channel.send("そんなバカな！！");
-    } else if (isNaN(times) || times == 0) {
-        channel.send('だが断る');
-    }
-    else if (times > 665 || times < -665) {
-        channel.send("Artificial JoJo: Discord, what does the scouter say about the character count?");
-        channel.send("Discord: It's over 2000!!!");
-    }
-    else if (times == 4) {
-        channel.send(boldRepetitive("オラ ", 5));
-    }
-    else if (times > 0) {
-        channel.send(boldRepetitive("オラ ", times));
-    }
-    else if (times < 0) {
-        channel.send(boldRepetitive("無駄 ", -times));
-    }
-}
-
-// repeats the string in repeated a number of times in bold format
-function boldRepetitive(text, times) {
-    return "**" + text.repeat(times) + "**";
 }
 
 // gets the ranking of a person for a certain goods

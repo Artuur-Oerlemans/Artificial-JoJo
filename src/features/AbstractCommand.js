84af468cd23c42b10378379ac845f28314fe40de
Artuur <artuur.oerlemans@gmail.com>
2020-04-11 18:54:20 +0200

    improved abstract class

--- src/features/AbstractCommand.js
+++ src/features/AbstractCommand.js
@@ -3,19 +3,30 @@
 // but it's still good practice to act like it does
 class AbstractCommand {
 
-    public shouldCommandBeActivated(text) {
-        return False;
+    private const commandWord = "AbstractCommand"
+
+    public shouldCommandBeActivated(messageContent) {
+        var args = messageContent.split(' ');
+        var firstWord = args[0];
+
+        // now the first args will be the thing following the command
+        return firstWord == this.commandWord;
     }
 
     public activateCommand(message) {
+        throw "Command action not implemented for inherriting class";
 
     }
 
     public usageDescription() {
+        throw "usage description not implemented for inherriting class";
+
         return ";somethingSomething";
     }
 
     public commandDescription() {
+        throw "Command description not implemented for inherriting class";
+
         return "you can use this to do something"
     }
 

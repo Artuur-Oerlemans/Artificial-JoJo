9b0807367579a0582d53310aa065ea67d1be66b9
Artuur <artuur.oerlemans@gmail.com>
2020-04-12 18:35:07 +0200

    extracted help command

---
+++ src/commands/HelpCommand.js
@@ -0,0 +1,37 @@
+import AbstractCommand from "./AbstractCommand";
+import * as discord from "discord.js";
+
+class HelpCommand extends AbstractCommand {
+
+	// javascript won't allow normal class variables
+	static commandWord() { return "help"; }
+
+	static activateCommand(message) {
+		let channel = message.channel;
+		let embed = new discord.RichEmbed();
+
+		embed.setColor("FF5733");
+		embed.setThumbnail("https://vignette.wikia.nocookie.net/jjba/images/a/ab/Joseph-oh-my-god.jpg/revision/latest?cb=20140807173126");
+
+		embed.addField(";progress", "Shows how far I'm to becoming a gang-star");
+		embed.addField(";introduce", "I, Artificial JoJo, will give a short introduction to my dream.");
+		embed.addField(";my_stand", "Find out what your stand is.");
+		embed.addField(";loaves_eaten", "Tell how old you are and it calculates how many loaves you have approximately eaten.");
+		embed.addField(";ora", "Tell the amount of times you want ora");
+		embed.addField(";countdown", "Tells how long until the next episode.");
+		embed.addField(";taste_of_a", "What is this I'm tasting?\nExample: ;taste_of_a @user liar!");
+		embed.addField("contribute!", "In order to fullfil my dream of become a gang-star I need doekoe.\nTo help, mention me with any form of money.")
+
+		channel.send(embed);
+	}
+
+	static usageDescription() {
+		return commandWord();
+	}
+
+	static commandDescription() {
+		return "The command you used to get this text.";
+	}
+}
+
+export default HelpCommand;
--- src/service/Service.js
+++ src/service/Service.js
@@ -9,6 +9,7 @@ import makeChristmas from "../christmasMaker";
 import * as fs from "fs";
 import LoavesEatenCommand from "../commands/LoavesEatenCommand";
 import IntroductionCommand from "../commands/IntroductionCommand";
+import HelpCommand from "../commands/HelpCommand";
 
 //the thing that should be infront of commands
 const prefix = ";"
@@ -39,7 +40,7 @@ class Service{
 				shoutOra(message.channel, args[0]);
 				break;
 			case "help":
-				this.showHelp(message.channel);
+				HelpCommand.activateCommand(message);
 				break;
 			case "progress":
 				this.showProgress(message.channel);
@@ -62,30 +63,6 @@ class Service{
 		}
 		
 		this.executeDonations(message);
-	}
-
-	showIntroduction(channel) {
-		channel.send("I, Artificial JoJo, have a dream");
-		channel.send("I want to become a gang-star");
-		channel.send("Hence have to attain the rank of capo");
-		channel.send("Will YOU help me gather 10,000,000,000 lire?");
-	}
-
-	showHelp(channel) {
-		let embed = new discord.RichEmbed();
-		embed.setColor("FF5733");
-		embed.setThumbnail("https://vignette.wikia.nocookie.net/jjba/images/a/ab/Joseph-oh-my-god.jpg/revision/latest?cb=20140807173126");
-
-		embed.addField(";progress", "Shows how far I'm to becoming a gang-star");
-		embed.addField(";introduce", "I, Artificial JoJo, will give a short introduction to my dream.");
-		embed.addField(";my_stand", "Find out what your stand is.");
-		embed.addField(";loaves_eaten", "Tell how old you are and it calculates how many loaves you have approximately eaten.");
-		embed.addField(";ora", "Tell the amount of times you want ora");
-		embed.addField(";countdown", "Tells how long until the next episode.");
-		embed.addField(";taste_of_a", "What is this I'm tasting?\nExample: ;taste_of_a @user liar!");
-		embed.addField("contribute!", "In order to fullfil my dream of become a gang-star I need doekoe.\nTo help, mention me with any form of money.")
-
-		channel.send(embed);
 	}
 
 	showCountdownNextEpisode(channel) {

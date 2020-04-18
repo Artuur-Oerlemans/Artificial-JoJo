c0fffce2e3deb6a22bc25d1891ed60198e706507
Artuur <artuur.oerlemans@gmail.com>
2020-04-18 23:19:42 +0200

    remove all memory dependencies in donations

--- src/repository/donationRepository.js
+++ src/repository/donationRepository.js
@@ -20,6 +20,35 @@ class DonationRepository {
 
 		return leaderBoard;
 	}
+
+	// change the quantity of a certain goods in the JSON file.
+	updateInventory(contributor, goods, change) {
+		// update the inventory value
+		let inBank = this.memory["inventory"][goods];
+		let afterTransfer = inBank + change;
+		this.memory["inventory"][goods] = afterTransfer;
+		// register the contribution
+		let displayName = contributor.lastMessage.member.displayName;
+		console.log(displayName);
+		// check if the user is already in the database or update display name
+		if (!this.memory["contributors"][contributor.id]) {
+			this.memory["contributors"][contributor.id] = { displayName: displayName };
+		} else {
+			this.memory["contributors"][contributor.id]["displayName"] = displayName;
+		}
+
+		// check if previous contributions have already with these goods
+		let afterContribution = this.memory["contributors"][contributor.id][goods] ? this.memory["contributors"][contributor.id][goods] + change : change;
+
+		this.memory["contributors"][contributor.id][goods] = afterContribution;
+
+		// sync to memory.json
+		fs.writeFile("./src/memory.json", JSON.stringify(this.memory, null, 4), err => {
+			if (err) throw err;
+			console.log(JSON.stringify(this.memory, null, 4));
+		});
+		return afterTransfer;
+	}
 }
 
 export default DonationRepository;
--- src/service/Donations.js
+++ src/service/Donations.js
@@ -16,8 +16,6 @@ class Donations {
 			message.channel.send(this.thankYouMessage(message.author));
 		message.channel.send(currency.description);
 		this.receiveDonation(message.author, message.channel, currency.valueInLires);
-
-
 	}
 
 	// Donate lires
@@ -25,7 +23,7 @@ class Donations {
 		let inBank = this.repository.moneyInBank()
 		let afterTransfer = inBank;
 		if (change != 0) {
-			afterTransfer = this.updateInventory(contributor, "lires", change);
+			afterTransfer = this.repository.updateInventory(contributor, "lires", change);
 		}
 
 		channel.send(inBank + " lire => " + afterTransfer + " lire");
@@ -55,35 +53,6 @@ class Donations {
 		let random = Math.floor(Math.random() * responses.length);
 
 		return responses[random];
-	}
-
-	// change the quantity of a certain goods in the JSON file.
-	updateInventory(contributor, goods, change) {
-		// update the inventory value
-		let inBank = this.repository.memory["inventory"][goods];
-		let afterTransfer = inBank + change;
-		this.repository.memory["inventory"][goods] = afterTransfer;
-		// register the contribution
-		let displayName = contributor.lastMessage.member.displayName;
-		console.log(displayName);
-		// check if the user is already in the database or update display name
-		if (!this.repository.memory["contributors"][contributor.id]) {
-			this.repository.memory["contributors"][contributor.id] = { displayName: displayName };
-		} else {
-			this.repository.memory["contributors"][contributor.id]["displayName"] = displayName;
-		}
-
-		// check if previous contributions have already with these goods
-		let afterContribution = this.repository.memory["contributors"][contributor.id][goods] ? this.repository.memory["contributors"][contributor.id][goods] + change : change;
-
-		this.repository.memory["contributors"][contributor.id][goods] = afterContribution;
-
-		// sync to memory.json
-		fs.writeFile("./src/memory.json", JSON.stringify(this.repository.memory, null, 4), err => {
-			if (err) throw err;
-			console.log(JSON.stringify(this.repository.memory, null, 4));
-		});
-		return afterTransfer;
 	}
 	
 	getRanking(id) {
